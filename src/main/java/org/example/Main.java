e1da920e29cb863822d9f4dd3513e08792c9c8c5
Nicolas Leroux <hertford.fr@gmail.com>
2024-09-24 11:34:03 +0200

    kata001 : remove active commentary

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,8 +14,6 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         ArrayList<Movie> recentMovies = createRecentMovies();
 
         List<Movie> foundedMovies = filterByTitle(recentMovies, "the");
